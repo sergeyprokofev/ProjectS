ccc86ec49e866da295e9f543a7dcd246171e360a
Sergey Prokofev <sergey.prokofev@gmail.com>
2018-03-13 12:51:28 +0700

    13032017.12:51

--- Spirt_STM32.v8/Src/bak/eeprom_i2c.h
+++
@@ -1,6 +0,0 @@
-void EE_init(I2C_TypeDef* I2Cx, uint32_t speed);
-void EE_single_write(I2C_TypeDef* I2Cx, uint8_t HW_address, uint8_t addr, uint8_t data);
-void EE_burst_write(I2C_TypeDef* I2Cx, uint8_t HW_address, uint8_t addr, uint8_t n_data, uint8_t *data);
-uint8_t EE_single_read(I2C_TypeDef* I2Cx, uint8_t HW_address, uint8_t addr);
-void EE_burst_read(I2C_TypeDef* I2Cx, uint8_t HW_address, uint8_t addr, uint8_t n_data, uint8_t *data);
-
